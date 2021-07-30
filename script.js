c0eac71f6579f73b46822700ade8b47f6bd2936a
Alena DENIS <basstet067@yahoo.com>
2021-07-30 17:55:19 +0200

    fix(script.js): fixed mistakes

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 'use strict';
 
-let money = 2000,
-    income = 'фриланс',
-    addExpenses = 'налоги, Аренда, страховка, связь',
-    deposit = true,
-    mission = 50000,
-    period = 12,
-    budgetDay;
+const money = 2000,
+  income = 'фриланс',
+  addExpenses = 'налоги, Аренда, страховка, связь',
+  deposit = true,
+  mission = 50000,
+  period = 12,
+  budgetDay = money / 30;
 
-console.log('typeof money: ', typeof money);
-console.log('typeof income: ', typeof income);
-console.log('typeof deposit: ', typeof deposit);
-console.log('addExpenses.length: ', addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель - заработать ' + mission + ' евро');
-
-addExpenses = addExpenses.toLowerCase();
-addExpenses = addExpenses.split(', ');
-console.log('addExpenses: ', addExpenses);
-
-budgetDay = money / 30;
-console.log('budgetDay: ', budgetDay);
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель - заработать ${mission} евро`);
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
